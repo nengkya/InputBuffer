4364917f733454c253c07da212e8ad63eb17e00f
nengkya <nengkya@yahoo.com>
2025-09-01 17:03:17 +0700

    https://cstack.github.io/db_tutorial/parts/part1.html

---
+++ git_remote.c
@@ -0,0 +1,3 @@
+/*
+in git, a "remote" refers to a link or connection to another Git repository, typically hosted on a server (like GitHub, GitLab, or Bitbucket)
+*/
---
+++ git_remote_add.c
@@ -0,0 +1,13 @@
+/*
+the git remote add command in git is used to establish a connection between your local git repository and a remote repository.
+this allows you to interact with the remote repository, such as pushing your local changes to it or pulling changes from it.
+Here's a breakdown of what it means:
+git remote: This part of the command refers to managing remote repositories.
+add: This signifies that you are adding a new remote connection.
+<name>: This is a user-defined name that you assign to the remote repository.
+By convention, the primary remote is often named "origin," but you can choose any descriptive name.
+<url>: This is the URL of the remote repository, which specifies its location (e.g., on GitHub, GitLab, Bitbucket, or a private server).
+Example:
+Code
+git remote add origin https://github.com/your-username/your-repository.git
+*/
